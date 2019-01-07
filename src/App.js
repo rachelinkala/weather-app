f5d5a25fd0f1175e4cdd41d2f532a3a149b63887
Rachel Inkala <rachelinkala@gmail.com>
2019-01-07 14:52:05 -0600

    fetch weather api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Form from './components/Form';
 import Weather from './components/Weather';
 
 class App extends React.Component {
+  getWeather = async () => {
+    const API_KEY = process.env.WEATHER_API_KEY;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
     <div>
