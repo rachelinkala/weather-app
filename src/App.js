daec80d96ace4a052d8724199d17eebe983a6a91
Rachel Inkala <rachelinkala@gmail.com>
2019-01-07 15:40:27 -0600

    api fix

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Form from './components/Form';
 import Weather from './components/Weather';
 
 class App extends React.Component {
-  getWeather = async () => {
-    const API_KEY = process.env.WEATHER_API_KEY;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`);
+  getWeather = async (e) => {
+    e.preventDefault()
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log(data);
   }
@@ -14,7 +15,7 @@ class App extends React.Component {
     return (
     <div>
       <Titles />
-      <Form />
+      <Form getWeather={this.getWeather} />
       <Weather />
     </div>
     );
