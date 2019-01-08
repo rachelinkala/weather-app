dcc107e08c1d9b3ffde274b9fafda7c68e0d87b4
Rachel Inkala <rachelinkala@gmail.com>
2019-01-08 12:34:03 -0600

    error message

--- src/App.js
+++ src/App.js
@@ -20,15 +20,26 @@ class App extends React.Component {
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value."
+      });
+    }
   }
 
   render() {
