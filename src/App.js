b288a713851b48fccdac1d164c96fc9f6a50ff20
Rachel Inkala <rachelinkala@gmail.com>
2019-01-08 12:02:13 -0600

    state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import Form from './components/Form';
 import Weather from './components/Weather';
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
@@ -12,7 +21,16 @@ class App extends React.Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
   }
+
   render() {
     return (
     <div>
